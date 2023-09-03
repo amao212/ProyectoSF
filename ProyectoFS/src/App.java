1187670dd0183b59f59117efabba28fe1a2b420d
Angel Anguaya <105741734+amao212@users.noreply.github.com>
2023-09-02 19:10:30 -0500

    Se eliminó una carpeta innecesaria

--- ProyectoFS/src/App.java
+++ ProyectoFS/src/App.java
@@ -4,7 +4,7 @@ import UserInterface.panatallaCarga;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        
+         
         panatallaCarga.mostrarPantallaDeCargaP();
         InicioDeSesion.mostrarInicio();
     }
